9723e432b28be1c3587d5e78cb0ed057438c054a
Kamlesh Yadav <kamlesh1089@gmail.com>
2026-07-30 22:50:22 +0530

    Add GA4 analytics tracking

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
-import { Routes, Route } from 'react-router'
+import { useEffect } from 'react'
+import { Routes, Route, useLocation } from 'react-router'
 import Home from './pages/Home'
 import Pricing from './pages/Pricing'
 
+declare global {
+  interface Window {
+    gtag?: (...args: unknown[]) => void
+  }
+}
+
+function AnalyticsPageViews() {
+  const location = useLocation()
+
+  useEffect(() => {
+    window.gtag?.('event', 'page_view', {
+      page_title: document.title,
+      page_location: window.location.href,
+      page_path: `${location.pathname}${location.search}`,
+    })
+  }, [location.pathname, location.search])
+
+  return null
+}
+
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/pricing" element={<Pricing />} />
-    </Routes>
+    <>
+      <AnalyticsPageViews />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pricing" element={<Pricing />} />
+      </Routes>
+    </>
   )
 }
